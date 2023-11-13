e6715f9881cd0a464eec8e420ea108774b7e1262
Ivan Stanev <122620073+StanevIvan@users.noreply.github.com>
2023-11-13 12:46:00 +0200

    Delete web directory

--- web/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
-
-
-def test(request):
-    return render(request, 'home.html')  # Assuming you have a template named 'home.html'
